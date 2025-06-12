915a2e213c2a4c9b8b130eebf402cc5c773cb4e4
Vladyslav <mvi110601@gmail.com>
2025-06-12 23:45:15 +0300

    Updated boostrap path

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once '..' . DIRECTORY_SEPARATOR . 'config.php';
+include_once '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'config.php';
 
 spl_autoload_register(function ($className){
     $classPath = '../' . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
