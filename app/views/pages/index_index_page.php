67807d573c21dbcc4f55cf13ec9c34b4c50c364d
kravetso <okravets.info@gmail.com>
2025-06-12 13:26:49 +0300

    create pages,templates,css

---
+++ app/views/pages/index_index_page.php
@@ -0,0 +1,9 @@
+<div><a href="index_upload_page.php">Add Photo</a></div>
+<div>Photo</div>
+<div>
+    <form action="#" method="post">
+        <input type="hidden" name="imageId" value="id">
+        <input type="submit" value="Like">
+    </form>
+</div>
+<div>Pagination</div>
---
+++ app/views/pages/index_upload_page.php
@@ -0,0 +1,9 @@
+<form action="#" method="post" enctype="multipart/form-data">
+    <label for="images">Вибрати файл: </label>
+    <input type="file" name="image" id="images">
+    <input type="submit" value="Ok">
+
+    <div class="errors">
+        <p>Помилки</p>
+    </div>
+</form>
