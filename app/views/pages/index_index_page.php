ecd6669dda66ae30d03995c138074523928c446a
kravetso <okravets.info@gmail.com>
2025-06-13 00:05:00 +0300

    add route()

--- app/views/pages/index_index_page.php
+++ app/views/pages/index_index_page.php
@@ -6,7 +6,7 @@
         <input type="submit" value="Like">
     </form>
 </div>
-<div class="add-photo"><a href="index_upload_page.php">Add Photo</a></div>
+<div class="add-photo"><a href="<?= \app\core\Route::url('photocontroller', 'upload')?>">Add Photo</a></div>
 <div class="pagination">
     <a href="#">&laquo;</a>
     <a href="#">1</a>
