b878aee8c79cb524b693d6569fd828eefee9f764
kravetso <okravets.info@gmail.com>
2025-06-13 14:05:18 +0300

    pagination added

--- app/views/pages/index_index_page.php
+++ app/views/pages/index_index_page.php
@@ -16,10 +16,18 @@
 <div class="add-photo"><a href="<?= \app\core\Route::url('photo', 'upload')?>">Add Photo</a></div>
 
 <div class="pagination">
-    <a href="#">&laquo;</a>
-    <a href="#">1</a>
-    <a class="active" href="#">2</a>
-    <a href="#">3</a>
-    <a href="#">4</a>
-    <a href="#">&raquo;</a>
+    <?php if ($prevPage > 0): ?>
+        <a href="<?= \app\core\Route::url('photo', 'index') ?>page=<?= $prevPage ?>">&laquo;</a>
+    <?php endif; ?>
+
+    <?php for ($i = 1; $i <= $pageCount; $i++): ?>
+        <a href="<?= \app\core\Route::url('photo', 'index') ?>page=<?= $i ?>"
+           class="<?= ($i == $page) ? 'active' : '' ?>">
+            <?= $i ?>
+        </a>
+    <?php endfor; ?>
+
+    <?php if ($nextPage <= $pageCount): ?>
+        <a href="<?= \app\core\Route::url('photo', 'index') ?>page=<?= $nextPage ?>">&raquo;</a>
+    <?php endif; ?>
 </div>
