eccc8ff5e8c83df30937b2f09cb7b94d9467e900
kravetso <okravets.info@gmail.com>
2025-06-15 01:56:29 +0300

    tag img changed

--- app/views/pages/index_index_page.php
+++ app/views/pages/index_index_page.php
@@ -1,14 +1,15 @@
 <?php foreach ($photos as $photo):?>
     <div class="photo">
         <div class="id"><?= $photo['id']?></div>
-        <img src="<?= $photo['path']?>" alt="img_name">
+        <img src="<?= PHOTO_UPLOAD_URL . $photo['name']?>" alt="<?= $photo['name']?>">
     </div>
 
     <div class="likes">
         <div><?= $photo['likes']?></div>
         <form action="<?=\app\core\Route::url('like', 'like')?>" method="post">
             <input type="hidden" name="imageId" value="<?= $photo['id']?>">
-            <input type="submit" value="Like">
+            <input type="submit" value="Like" id ="like">
+            <div><a href="#like"></a></div>
         </form>
     </div>
 <?php endforeach;?>
