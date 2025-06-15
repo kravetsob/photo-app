ee882dab5050e7902ddc83d147c389a2f74c82d1
Vladyslav <mvi110601@gmail.com>
2025-06-15 19:22:52 +0300

    Added input file validation check

--- app/services/PhotoService.php
+++ app/services/PhotoService.php
@@ -2,6 +2,7 @@
 
 namespace app\services;
 
+use app\core\Route;
 use app\models\PhotoModel;
 
 class PhotoService {
@@ -12,15 +13,28 @@ class PhotoService {
         $this->photoModel = new PhotoModel();
     }
 
-    public function upload(array $file)     
+    public function upload(array $file)
     {
         //TODO Validation
+        if ($file['size'] === 0) {
+            return FILE_UPLOAD_ERR[24];
+        }
+        if (!in_array($file['type'], PHOTO_AVAILABLE_TYPES, true)){
+            return FILE_UPLOAD_ERR[25];
+        }
+
+// 2. Перевірка розміру вручну
+        if ($file['size'] > PHOTO_MAX_FILE_SIZE) {
+            return FILE_UPLOAD_ERR[2];
+        }
 
         //Збереження на диск
         $newName = uniqid() . '_' . basename($file['name']);
         move_uploaded_file($file['tmp_name'],  PHOTO_UPLOAD_DIR . DIRECTORY_SEPARATOR .$newName);
 
         $this->photoModel->upload($newName);
+
+        return null;
     }
 
 }
