5b0ef6177811a7bc1f224a9efbdf137598c3ab8e
kravetso <okravets.info@gmail.com>
2025-06-18 00:50:11 +0300

    delete new View

--- app/controllers/LikeController.php
+++ app/controllers/LikeController.php
@@ -2,7 +2,6 @@
 
 namespace app\controllers;
 
-use app\core\View;
 use app\core\Route;
 use app\models\LikeModel;
 use app\core\Page;
@@ -15,17 +14,11 @@ class LikeController
     private LikeModel $model; //will contain a LikeModel object that performs queries to the database.
 
     /**
-     * @var View
-     */
-    private View $view; //will be used to display HTML pages.
-
-    /**
      * LikeController constructor.
      */
     public function __construct()
     {
         $this->model = new LikeModel();
-        $this->view = new View();
     }
 
     /**
