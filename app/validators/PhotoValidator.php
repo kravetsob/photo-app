f5cedcd62defac149101520a1a0b7f75c3495270
kravetso <okravets.info@gmail.com>
2025-06-18 11:30:52 +0300

    class photoValidator created

---
+++ app/validators/PhotoValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\validators;
+
+class PhotoValidator
+{
+    /**
+     * Checks if a file fits the upload conditions.
+     *
+     * @param array $file
+     * @return array
+     */
+    public function validate(array $file): array
+    {
+        $errors = [];
+
+        if (!isset($file) || $file['error'] !== UPLOAD_ERR_OK) {
+            $errors[] = FILE_UPLOAD_ERRORS[$file['error']] ?? 'Невідома помилка завантаження.';
+            return $errors;
+        }
+
+        // MIME-тип
+        if (!in_array($file['type'], PHOTO_AVAILABLE_TYPES, true)) {
+            $errors[] = FILE_UPLOAD_ERRORS[5]; // Недопустимий тип файла
+        }
+
+        // Розмір
+        if ($file['size'] > PHOTO_MAX_FILE_SIZE) {
+            $errors[] = FILE_UPLOAD_ERRORS[2]; // Завеликий файл
+        }
+
+        return $errors;
+    }
+}
