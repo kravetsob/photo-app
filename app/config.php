d057d99b9500cdc08d1bb52e8edc95c0df442c4a
kravetso <okravets.info@gmail.com>
2025-06-18 11:50:42 +0300

    renamed FILE_UPLOAD_ERR on FILE_UPLOAD_ERRORS

--- app/config.php
+++ app/config.php
@@ -1,6 +1,5 @@
 <?php
 const SITE_NAME = 'PhotoGraphy';
-// const PHOTO_UPLOAD_DIR = __DIR__ . '/../public/storage/';
 const PHOTO_UPLOAD_DIR = 'storage/';
 const PHOTO_UPLOAD_URL = 'storage/';
 const PHOTO_AVAILABLE_TYPES = [
@@ -11,7 +10,7 @@ const PHOTO_AVAILABLE_TYPES = [
     'image/webp',
 ];
 const PHOTO_MAX_FILE_SIZE = 1024 * 1024 * 5;
-const FILE_UPLOAD_ERR = [
+const FILE_UPLOAD_ERRORS = [
     0 => 'Файл успішно завантажено.',
     1 => 'Завантажений файл перевищує максимальний розмір, вказаний у параметрі upload_max_filesize в php.ini.',
     2 => 'Завантажений файл перевищує допустимий розмір файлу. Оберіть файл до 5МБ.',
